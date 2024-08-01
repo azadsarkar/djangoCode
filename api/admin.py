19ad73855ae7f16f3fb874edf3fc20cad18813f5
azad001 <azadsarkar7617@yopmail.com>
2024-08-01 12:18:13 +0530

    Add post methon

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from api.models import Student
+from api.models import *
 
-# Register your models here.
-@admin.register(Student)
-class studentAdmin(admin.ModelAdmin):
-    list_display=["name","email","age","gender"]
+
+# studentcommon ke andr hi studentprole ko add kr skte hai tabular form me
+# @admin.register(StudentProfile)
+class StudentProfileAdmin(admin.TabularInline):
+    model = StudentProfile
+
+@admin.register(StudentCommon)
+class StudentCommonAdmin(admin.ModelAdmin):
+    list_display=["name","last_name","school_name","school_code"]
+    inlines = [StudentProfileAdmin] # StudentProfile instance , StudentCommon model ke andr hi show hoga admin pannel pr 
+    
+
+@admin.register(Subject)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=["subject_name"]
+    
+@admin.register(StudentSubject)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=["student", "subject"]
+    
